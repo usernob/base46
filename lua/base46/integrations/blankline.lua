619368a05e28765f5ca1cb01ce8697c2b1de7b10
siduck <siduck@tutanota.com>
2024-03-14 11:16:24 +0530

    add ibl.scope.underline.1 hlgroup

--- lua/base46/integrations/blankline.lua
+++ lua/base46/integrations/blankline.lua
@@ -2,6 +2,6 @@ local colors = require("base46").get_theme_tb "base_30"
 
 return {
   IblChar = { fg = colors.line },
-  IblScopeChar = { fg = colors.grey }, -- first indenline in scope only
-  IblScopeFirstLine = { bg = colors.one_bg2 },
+  IblScopeChar = { fg = colors.grey },
+  ["@ibl.scope.underline.1"] = { bg = colors.black2 },
 }
